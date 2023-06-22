6a0703ee11ac87f010ca2a1c2fb49aac701057a6
vasumathi298 <vasumathi.n29@gmail.com>
2023-06-21 22:59:00 -0400

    first commit

---
+++ src/components/UserPlaylists/component.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./UserPlaylists.css";
+
+class UserPlaylists extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.userId !== "" && nextProps.token !== "" 
+) {
+      this.props.fetchPlaylistsMenu(nextProps.userId, nextProps.token);
+    }
+  }
+
+  renderPlaylists() {
+    return this.props.playlistMenu.map(playlist => {
+      const getPlaylistSongs = () => {
+        this.props.fetchPlaylistSongs(
+          playlist.owner.id,
+          playlist.id,
+          this.props.token
+        );
+        this.props.updateHeaderTitle(playlist.name);
+      };
+      
+
+      return (
+        <li
+          onClick={getPlaylistSongs}
+          className={
+            this.props.title === playlist.name
+              ? "active side-menu-item"
+              : "side-menu-item"
+          }
+          key={playlist.id}
+        >
+          {playlist.name}
+        </li>
+      );
+    });
+  }
+  
+
+  render() {
+    const addPlaylist =()=>{
+      console.log("owner id");
+      console.log(this.props.userId);
+      const playlistName = prompt("Enter the playlist name:");
+      this.props.createPlaylist (
+        this.props.userId,
+        this.props.token,
+        playlistName
+      );
+      this.props.fetchPlaylistsMenu(this.props.userId, this.props.token);
+
+    };
+    return (
+      <div className="user-playlist-container">
+        <h3 className="user-playlist-header">Playlists <span className="add-icon" onClick={addPlaylist}>+</span></h3>
+         {this.props.playlistMenu && this.renderPlaylists()}
+      </div>
+    );
+  }
+}
+
+UserPlaylists.propTypes = {
+  userId: PropTypes.string,
+  token: PropTypes.string,
+  title: PropTypes.string,
+  playlistMenu: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  fetchPlaylistsMenu: PropTypes.func,
+  fetchPlaylistSongs: PropTypes.func,
+  updateHeaderTitle: PropTypes.func,
+  createPlaylist: PropTypes.func,
+};
+
+export default UserPlaylists;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//create the redux store
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
